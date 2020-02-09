4967c16604115e1b28d73de371aac3a378388dc0
DenkyOgbonnaya <denkyogb@gmail.com>
2020-02-09 15:19:56 +0100

    Add auth token to request header

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -1,5 +1,30 @@
-import ApolloClient from "apollo-boost";
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  HttpLink
+} from "apollo-boost";
 
-export default new ApolloClient({
-  uri: "http://localhost:8000/graphql"
+const httpLink = new HttpLink({ uri: "http://localhost:8000/graphql" });
+
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authorization token from local storage.
+  const token = localStorage.getItem("token");
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  });
+
+  // Call the next link in the middleware chain.
+  return forward(operation);
 });
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink), // Chain it with the HttpLink
+  cache: new InMemoryCache()
+});
+
+export default client;
