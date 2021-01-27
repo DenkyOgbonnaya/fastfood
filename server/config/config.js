124de76d9225f63e938ef6bbeea716fbf7fc8d56
DenkyOgbonnaya <denkyogb@gmail.com>
2021-01-27 04:57:57 +0100

    Add db url env variable

--- server/config/config.js
+++ server/config/config.js
@@ -6,24 +6,25 @@ const{
   DB_HOST,
   DB_PORT,
   DB_TEST_URL,
-  DB_URL
+  DB_URL,
+  DATABASE_URL
 } = process.env;
-//postgres://uhcdgtlbakuggo:c616ad5c14f965c15bafacc897ce4f36e1f6a147804a26d63d54e5c3d225bb27@ec2-46-137-79-235.eu-west-1.compute.amazonaws.com:5432/d4ugoaplgbc0in
 module.exports = {
   development: {
-    username: 'postgres',//DB_USER,
+    /*username: 'postgres',//DB_USER,
     database: 'fastfood',//DB_NAME,
     password: 'denky', //DB_PASS
     port: DB_PORT,
-    host: DB_HOST,
+    host: DB_HOST,*/
+    use_env_variable: DATABASE_URL,
     dialect: 'postgres'
   },
   test: {
-    use_env_variable: DB_TEST_URL,
+    use_env_variable: DATABASE_URL,
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: DB_URL,
+    use_env_variable: DATABASE_URL,
     dialect: 'postgres'
   }
 };
